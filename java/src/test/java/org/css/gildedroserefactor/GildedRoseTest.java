0d82f8ff51bcec51282cf05c5f6151951ee00ab0
Tony Vo <ttrung.vo@gmail.com>
2019-06-15 09:57:19 -0600

    fixed test

--- java/src/test/java/org/css/gildedroserefactor/GildedRoseTest.java
+++ java/src/test/java/org/css/gildedroserefactor/GildedRoseTest.java
@@ -1,7 +1,8 @@
 package org.css.gildedroserefactor;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
 
